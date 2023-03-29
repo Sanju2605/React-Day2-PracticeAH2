5de512a1a190771fbcda8a5b4fc2bbf9cb697dce
Sanju2605 <108916675+Sanju2605@users.noreply.github.com>
2023-03-29 10:32:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Count from './Count';
+
+
+function App() {
+  return (
+    <div >
+      <center>
+    <Count/>
+    </center>
+    </div>
+  );
+}
+
+export default App;
